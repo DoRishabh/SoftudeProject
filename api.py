eecd8373ca902224d6c67f1dbf2c1f458e19c708
Rishabh Mehta <rishabhmjhail@gmail.com>
2026-06-05 05:53:24 +0530

    Update api.py

--- api.py
+++ api.py
@@ -22,8 +22,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-GROUP_ID    = ""
-REPORT_ID   = "3c26f1ff-f038-4841-b0ef-cdc33b772805"  # page 1 - AI Query
+GROUP_ID = "72f110e5-6326-453f-9bdc-296521c12db6"
+REPORT_ID = "095cedfd-6e66-41cb-9a34-6b4c27b82f33"  # page 1 - AI Query
 REPORT_ID_2 = "4f1df241-4d62-4774-bb25-8af18d1e553f"  # page 2 - Dynamic Slicer
 _3 = "1123e3e7-5f3a-4499-b1ca-0c37f673f29a"  # page 3 - US Map
 _4 = "f109fb78-c9fd-472d-8747-040f4ffc0daa"  # page 4 - Interactive Sales
